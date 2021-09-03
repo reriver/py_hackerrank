24399d38e7c1bce415477070850c6cf92a62ff2e
Jorge <71904707+reriver@users.noreply.github.com>
2021-09-03 19:05:01 +0300

    nested lists done

--- nested_lists_full.py
+++ nested_lists_full.py
@@ -8,14 +8,32 @@ if __name__ == '__main__':
         mark = float(input())
         marks.append(mark)
         st = [mark, name]
+        # marks[mark] = name
+        # students.append([input(), input()])
+        # students[mark] = name
         students.append(st)
-    marks.sort()
-    second_lowest_mark = marks[1]
 
+    print(marks)
+    marks.sort()
+    print(marks)
+    print(marks[1])
+    second_lowest_mark = marks[1]
+    print(students)
     output = []
+    # for v in students:
+    #     if marks[1] == v[0]:
+    #         print(v[1])
     for key, value in students:
         if key == second_lowest_mark:
+            print(value)
             output.append(value)
 
     output.sort()
+    print(output)
     print('\n'.join(output))
+    #
+    # marks = []
+    # for i in range(n):
+    #     marks.append([input(), input()])
+    #
+    # print(marks)
