dfb90f10f7a833a67755ecb8f425256f7baf4de3
Jorge <71904707+reriver@users.noreply.github.com>
2021-09-26 10:21:48 +0300

    day 20 done

--- day19.py
+++ day19.py
@@ -1,18 +1,20 @@
 class AdvancedArithmetic(object):
-    def divisorSum(n):
+    def divisor_sum(self, n):
         raise NotImplementedError
 
+
 class Calculator(AdvancedArithmetic):
-    def divisorSum(self, n):
+    def divisor_sum(self, n):
         ret = n
         for i in range(1, n):
             if n % i == 0:
                 ret += i
         return ret
 
+
 if __name__ == '__main__':
     n = int(input())
     my_calculator = Calculator()
-    s = my_calculator.divisorSum(n)
+    s = my_calculator.divisor_sum(n)
     print("I implemented: " + type(my_calculator).__bases__[0].__name__)
     print(s)
---
+++ day20.py
@@ -0,0 +1,23 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    a = list(map(int, input().rstrip().split()))
+    swaps = 0
+    for i in range(n - 1):
+        for j in range(0, n - i - 1):
+            if a[j] > a[j + 1]:
+                a[j], a[j + 1] = a[j + 1], a[j]
+                swaps += 1
+    print("Array is sorted in", swaps, "swaps.")
+    print("First Element:", a[0])
+    print("Last Element:", a[len(a) - 1])
